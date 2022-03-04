fe62ba5c81ec9874e36463063dc70e6807202c7a
Kevin Sullivan <sullivan@virginia.edu>
2022-03-04 16:23:48 +0000

    Update bool_lang

--- src/content/project_1/bool_lang.lean
+++ src/content/project_1/bool_lang.lean
@@ -4,9 +4,27 @@ namespace hidden
 
 -- DATA TYPE
 
+/-
+inductive bool_var 
+| X
+| Y 
+| Z
+-/
+
+inductive bool_var 
+| V (n : ℕ)
+
+open bool_var
+
+-- Test
+def X := V 0
+def Y := V 1
+def Z := V 2
+
 inductive bool_lang : Type
 | TT : bool_lang
 | FF : bool_lang
+| var (v : bool_var) : bool_lang
 | conj (e1 e2 : bool_lang) : bool_lang
 | disj (e1 e2 : bool_lang) : bool_lang
 | neg (e : bool_lang)
@@ -14,25 +32,38 @@ inductive bool_lang : Type
 
 -- REFACTOR INTO TEST FILE
 open bool_lang
-
 def be1 := TT
 def be2 := FF
+def ve1 := var X
 def be3 := conj be1 be2
 def be4 := neg be3
 
 open boo
 
--- OPERATIONS
-def eval : bool_lang → boo
-| TT := tt
-| FF := ff
-| (conj e1 e2) := and (eval e1) (eval e2)
-| (disj e1 e2) := or (eval e1) (eval e2)
-| (neg e) := not (eval e)
+def var_interp_1 : bool_var → boo
+| v := boo.tt
 
--- REFACTOR INTO TEST FILE
-#reduce eval be4
-#reduce eval (conj (disj be2 be4) be3)
+/-
+def var_interp_2 : bool_var → boo
+| X := tt
+| Y := ff
+| Z := tt
+-/
+
+def var_interp_2 : bool_var → boo
+| (V 0) := tt
+| (V 1) := ff
+| (V 2) := tt
+| _ := tt
+
+-- OPERATIONS
+def eval : bool_lang → (bool_var → boo) → boo
+| TT _ := tt
+| FF _ := ff
+| (var v) i := i v
+| (conj e1 e2) i := and (eval e1 i) (eval e2 i)
+| (disj e1 e2) i := or (eval e1 i) (eval e2 i)
+| (neg e) i := not (eval e i)
 
 -- NOTATIONS
 notation b1 * b2 := conj b1 b2
@@ -40,8 +71,62 @@ notation b1 + b2 := disj b1 b2
 prefix ! := neg
 
 -- REFACTOR INTO TEST FILE
+#reduce eval be4
+#reduce eval (conj (disj be2 be4) be3)
+#check X
+#check (var X)
+#reduce eval ((var X) * (var Y)) var_interp_1
+#reduce eval ((var X) * (var Y)) var_interp_2
+
+-- REFACTOR INTO TEST FILE
 #reduce eval ((be2 + be4) * be3)
 
+/-
+How do we know that eval terminates?
+-/
+
+-- Non-terminating functions would be fatal
+def bad : ℕ → false
+| n := bad n
+#check (bad 3)  -- Yikes a proof of false!
+
 -- PROPERTIES?
+
+/-
+A property of an expression, e, is that e
+evaluates to true. Let's call this property
+evaluates_to_true.
+-/
+def evaluates_to_true : bool_lang → Prop 
+| e := eval e = boo.tt
+
+example : evaluates_to_true TT :=
+begin
+  unfold evaluates_to_true,
+  trivial,
+end
+
+example : evaluates_to_true (TT * FF) :=
+begin
+  unfold evaluates_to_true,
+  simp [eval],
+  apply rfl,  -- false
+end
+
+-- Property of an individual expression
+example : evaluates_to_true (TT * TT) :=
+begin
+  unfold evaluates_to_true,
+  simp [eval],
+  apply rfl,  -- false
+end
+
+-- Property of the language as a whole: 
+-- evaluation is deterministic.
+
+-- HOMEWORK
+
+def eval_is_deterministic : _ := _
+
 
 end hidden
