1e66780cdf82b99771c0d5766ccd480e00f9a46f
Kevin Sullivan <sullivan@virginia.edu>
2022-03-31 16:34:05 +0000

    .

---
+++ src/assignments/hw6_sat/bool_sat_dev.lean
@@ -0,0 +1,15 @@
+import .bool_expr
+import .bool_semantics
+import .set_T
+
+namespace bool_lang
+open bool_lang.bool_expr
+
+-- Workspace for top-down development
+
+/-
+3. Review last time (SAT, lists, sets)
+4. Top-down structured programming!
+-/
+
+end bool_lang
